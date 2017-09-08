232236eba2b33d387eff3931a710e80d239d3116
Eric <makingentropy@gmail.com>
2017-09-08 17:35:45 -0500

    continuing work on front end layout/navigation functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,29 @@
 const app = angular.module("MyApp",[]);
 
-app.controller('mainController', ['$http', function($http){
+app.controller('userController', ['$http', function($http){
   const controller = this;
+  this.test="ANGULAR TEST SUCCESSFUL";
+  this.pageShowing='includes/dynamic_landing.html';
 
-  this.getIndex = function(){
-    $http({
-      method: 'GET',
-      url: '/'
-    }).then(function(response){
-      console.log(response, ' -- > this is the response');
-
-      controller.events = response.data;
-
-    }, function(error){
-      console.log("error on GET '/': ",error);
-    });
-  },
-
+  // this.getIndex = function(){
+  //   $http({
+  //     method: 'GET',
+  //     url: '/'
+  //   }).then(function(response){
+  //     console.log(response, ' -- > this is the response');
+  //
+  //     controller.events = response.data;
+  //
+  //   }, function(error){
+  //     console.log("error on GET '/': ",error);
+  //   });
+  // },
 
 }]);
+// fetch('http://localhost:3000/locations')
+// .then(response => response.json())
+// .then(json=>console.log(json))
+// .catch(err=>console.log(err))
+// window.onload = function() {
+//
+// }
--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app            = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
+app.listen(3001, ()=>{
 	console.log('listening....');
 });
