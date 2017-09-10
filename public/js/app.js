3a4f4150b3a5e584a43e2c54990289931616fe6a
Eric <makingentropy@gmail.com>
2017-09-10 03:44:28 -0500

    have my first  funciton GET-ing from the remote backend.  Unsure if I should add express-json

--- public/js/app.js
+++ public/js/app.js
@@ -5,21 +5,22 @@ app.controller('userController', ['$http', function($http){
   this.test="ANGULAR TEST SUCCESSFUL";
   this.pageShowing='includes/dynamic_landing.html';
 
-  // this.getIndex = function(){
-  //   $http({
-  //     method: 'GET',
-  //     url: '/'
-  //   }).then(function(response){
-  //     console.log(response, ' -- > this is the response');
-  //
-  //     controller.events = response.data;
-  //
-  //   }, function(error){
-  //     console.log("error on GET '/': ",error);
-  //   });
-  // },
+  this.gettasks= function(){
+    this.pageShowing='includes/dynamic_volunteer_allTasks.html';
+    console.log("clicked volunteer");
+    $http({
+      method: 'GET',
+      url: 'https://elderhelperappapi.herokuapp.com/tasks'
+    }).then(function(response){
+      console.log(response, ' -- > this is the response');
 
-}]);
+      controller.events = response.data;
+
+    }).catch(error=>console.log(error));
+  }
+
+}]); //end userController
+
 // fetch('http://localhost:3000/locations')
 // .then(response => response.json())
 // .then(json=>console.log(json))
