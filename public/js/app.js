ab964cdef0e4500b4aebe2028089d020a7b42068
Eric <makingentropy@gmail.com>
2017-09-10 05:17:15 -0500

    All tasks display after clicking on volunteer. Not yet excluding what tasks have been claimed.

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,30 @@
 const app = angular.module("MyApp",[]);
 
-app.controller('userController', ['$http', function($http){
+app.controller('rtController', ['$http', function($http){
   const controller = this;
   this.test="ANGULAR TEST SUCCESSFUL";
   this.pageShowing='includes/dynamic_landing.html';
 
   this.gettasks= function(){
+    //RENDER the following includes/*.html on index.html
     this.pageShowing='includes/dynamic_volunteer_allTasks.html';
+
     console.log("clicked volunteer");
     $http({
       method: 'GET',
       url: 'https://elderhelperappapi.herokuapp.com/tasks'
     }).then(function(response){
-      console.log(response, ' -- > this is the response');
+        console.log("typeOf response.data: ",typeof response.data);
+        console.log(response.data);
+        controller.allTasks=response.data;
+        console.log(controller.allTasks, ' -- > this is the response');
 
       controller.events = response.data;
 
     }).catch(error=>console.log(error));
   }
 
-}]); //end userController
+}]); //end rtontroller
 
 // fetch('http://localhost:3000/locations')
 // .then(response => response.json())
