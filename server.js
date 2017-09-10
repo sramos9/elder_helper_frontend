433a33b0e91f1037ec5ba3d72e3764e4e6c853e1
Eric <makingentropy@gmail.com>
2017-09-10 08:29:29 -0500

    added body-parser to server.js. Added non-functional button to dynamic_volunteer_oneTask.html

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express        = require('express');
-const app            = express();
+const express       = require('express');
+const app           = express();
+const bodyParser 		= require('body-parser');
 
+app.use(bodyParser.json()); //creates a property on request called req.body
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static('public'));
 
 
