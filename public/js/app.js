eb1c609b39b43f4b423f6d708f3c9462cec0c83c
Eric <makingentropy@gmail.com>
2017-09-11 06:53:37 -0500

    User can now see all the previous tasks written by an elder (from request menu branch)

--- public/js/app.js
+++ public/js/app.js
@@ -5,9 +5,17 @@ app.controller('rtController', ['$http', function($http){
   this.test="ANGULAR TEST SUCCESSFUL";
   this.pageShowing='includes/dynamic_landing.html';
   this.theVolunteerTask={};
+  this.theRequesterTask={};
+
   this.VolunteerTaskSelected=function(theTask){
     //RENDER the following includes/*.html on index.html
     this.pageShowing='includes/dynamic_volunteer_oneTask.html';
+    console.log("theTask: ",theTask);
+    this.theVolunteerTask=theTask;
+  };
+  this.RequesterTaskSelected=function(theTask){
+    //RENDER the following includes/*.html on index.html
+    this.pageShowing='includes/dynamic_requester_ReadOne_UD.html';
     console.log("theTask: ",theTask);
     this.theVolunteerTask=theTask;
   };
@@ -26,10 +34,43 @@ app.controller('rtController', ['$http', function($http){
         controller.allTasks=response.data;
         console.log(controller.allTasks, ' -- > this is the response');
 
-      controller.events = response.data;
-
     }).catch(error=>console.log(error));
-  }
+  };
+
+  this.getTasksByElder=function(){
+
+    console.log("submitted: ",controller.elderName);
+    $http({
+      method: 'GET',
+      url: 'https://elderhelperappapi.herokuapp.com/tasks'
+    }).then(function(response){
+      controller.allTasks=response.data;
+      console.log("number of tasks:",controller.allTasks.length);
+      controller.allTasksByElder=[];
+      for (let i = 0; i < controller.allTasks.length; i++) {
+        if(controller.allTasks[i].elder.name==controller.elderName){
+          console.log("Comparing: ",controller.allTasks[i].elder.name);
+          controller.allTasksByElder.push(controller.allTasks[i]);
+        }
+      }
+      if(controller.allTasksByElder.length==0){
+        controller.allTasksByElder=["No Results Found"];
+      }
+      console.log(controller.allTasksByElder);
+      //NOTE: ONLY RENDER PAGE AFTER .then() HAS ALL EXECUTED
+      //-since it runs asynchronously, dependent upon the data retrieval,
+      //  we reinitialize this.pageShowing here to make sure
+      //  all the data is available when the next .html snippet loads
+      //  RENDER the following includes/*.html on index.html
+      //-have to writer it as controller.pageShowing because this.pageShowing
+      //  is not properly in scope within the $html function
+      controller.pageShowing='includes/dynamic_requester_search_results.html';
+    }).catch(error=>console.log(error));
+    // //RENDER the following includes/*.html on index.html
+    // this.pageShowing='includes/dynamic_requester_search_results.html';
+    // console.log("switched pageShowing");
+  };
+
 
 }]); //end rtontroller
 
