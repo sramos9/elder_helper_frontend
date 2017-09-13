555bc64aca0ca1f00ef81d6badc9834cca194350
Samantha Ramos <samantharamos@samanthas-air.attlocal.net>
2017-09-13 09:21:56 -0500

    style

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express       = require('express');
 const app           = express();
-const bodyParser 		= require('body-parser');
-const PORT		=process.env.PORT || 3004;
+const bodyParser         = require('body-parser');
+const PORT        =process.env.PORT || 3004;
 
 app.use(bodyParser.json()); //creates a property on request called req.body
 app.use(bodyParser.urlencoded({extended:false}));
@@ -9,5 +9,5 @@ app.use(express.static('public'));
 
 
 app.listen(PORT, ()=>{
-	console.log('listening....');
+    console.log('listening....');
 });
